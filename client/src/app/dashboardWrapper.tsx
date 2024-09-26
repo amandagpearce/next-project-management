e3f60fa463cd16cb7dcf2c12ba7b7fc7071def71
Amanda Pearce <amandagpearce@gmail.com>
2024-09-26 19:30:30 +0100

    dark mode state

--- client/src/app/dashboardWrapper.tsx
+++ client/src/app/dashboardWrapper.tsx
@@ -6,7 +6,9 @@ import Sidebar from './(components)/Sidebar';
 import StoreProvider, { useAppSelector } from './redux';
 
 const DashboarLayout = ({ children }: { children: React.ReactNode }) => {
-  const isSidebarCollapsed = useAppSelector((state) => state.global.isSidebarCollapsed);
+  const isSidebarCollapsed = useAppSelector(
+    (state) => state.global.isSidebarCollapsed
+  );
   const isDarkMode = useAppSelector((state) => state.global.isDarkMode);
 
   // workaround since that cant be set on layout component level
@@ -17,14 +19,14 @@ const DashboarLayout = ({ children }: { children: React.ReactNode }) => {
     } else {
       document.documentElement.classList.remove('dark');
     }
-  })
+  });
 
   return (
     <div className="flex min-h-screen w-full bg-gray-50 text-gray-900">
       <Sidebar />
 
       <main
-        className={`flex w-full flex-col bg-gray-200 dark:bg-dark-bg ${isSidebarCollapsed ? '' :'md:pl-64'}`}
+        className={`flex w-full flex-col bg-gray-200 dark:bg-dark-bg ${isSidebarCollapsed ? '' : 'md:pl-64'}`}
       >
         <Navbar />
         {children}
@@ -34,11 +36,11 @@ const DashboarLayout = ({ children }: { children: React.ReactNode }) => {
 };
 
 const DashboardWrapper = ({ children }: { children: React.ReactNode }) => {
-    return (
-        <StoreProvider>
-            <DashboarLayout>{children}</DashboarLayout>
-        </StoreProvider>
-    );
+  return (
+    <StoreProvider>
+      <DashboarLayout>{children}</DashboarLayout>
+    </StoreProvider>
+  );
 };
 
 export default DashboardWrapper;
