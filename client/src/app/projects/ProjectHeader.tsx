63184621b5e629d6624effe173a4265e3ec837d8
Amanda Pearce <amandagpearce@gmail.com>
2024-10-12 17:05:02 +0100

    client: project header

--- client/src/app/projects/ProjectHeader.tsx
+++ client/src/app/projects/ProjectHeader.tsx
@@ -1,9 +1,23 @@
-import React from 'react';
+import Header from '@/components/Header';
+import React, { useState } from 'react';
 
-type Props = {};
+type Props = {
+  activeTab: string;
+  setActiveTab: (tabName: string) => void;
+};
 
-const ProjectHeader = (props: Props) => {
-  return <div>ProjectHeader</div>;
+const ProjectHeader = ({ activeTab, setActiveTab }: Props) => {
+  const [isModalNewProjectOpen, setIsModalNewProjectOpen] = useState(false);
+
+  return (
+    <div className="px-4 xl:px-6">
+      {/* Modal new project */}
+
+      <div className="py-6 lg:pb-4 lg:pt-8">
+        <Header name="Project Page" />
+      </div>
+    </div>
+  );
 };
 
 export default ProjectHeader;
